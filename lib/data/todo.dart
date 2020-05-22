e7f0148a2a18a79aab5d52fbfa7bd93dec1f8c95
Ketan <ketantoo@gmail.com>
2020-05-22 15:15:49 +0530

    App finished

--- lib/data/todo.dart
+++ lib/data/todo.dart
@@ -2,30 +2,31 @@ class Todo {
   int id;
   String name;
   String description;
-  String completedBy;
+  String completeBy;
   int priority;
+
   Todo({
     this.name,
     this.description,
-    this.completedBy,
+    this.completeBy,
     this.priority,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      "name": name,
-      "description": description,
-      "completedBy": completedBy,
-      "priority": priority,
+      'name': name,
+      'description': description,
+      'completeBy': completeBy,
+      'priority': priority,
     };
   }
 
   static Todo fromMap(Map<String, dynamic> map) {
     return Todo(
-      name: map["name"],
-      description: map["description"],
-      completedBy: map["completedBy"],
-      priority: map["priority"],
+      name: map['name'],
+      description: map['description'],
+      completeBy: map['completeBy'],
+      priority: map['priority'],
     );
   }
 }
--- lib/data/todo_db.dart
+++ lib/data/todo_db.dart
@@ -1,45 +1,36 @@
 import 'dart:async';
-import "package:path_provider/path_provider.dart";
-import "package:path/path.dart";
-import "package:sembast/sembast.dart";
-import "package:sembast/sembast_io.dart";
-
-import "todo.dart";
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+import 'todo.dart';
 
 class TodoDb {
+  //this needs to be a singleton
   static final TodoDb _singleton = TodoDb._internal();
+  //private internal constructor
   TodoDb._internal();
-/*
-A normal constructor returns a new instance of the current class. A factory
-constructor can only return a single instance of the current class: that's
-why factory constructors are often used when you need to implement the
-singleton pattern
 
- */
   factory TodoDb() {
     return _singleton;
   }
 
   DatabaseFactory dbFactory = databaseFactoryIo;
-
-  final store = intMapStoreFactory.store("todos");
-
+  final store = intMapStoreFactory.store('todos');
   Database _database;
 
-  Future<Database> get database async {
+  Future get database async {
     if (_database == null) {
-      await _openDb().then(
-        (db) {
-          _database = db;
-        },
-      );
+      await _openDb().then((db) {
+        _database = db;
+      });
     }
     return _database;
   }
 
   Future _openDb() async {
     final docsPath = await getApplicationDocumentsDirectory();
-    final dbPath = join(docsPath.path, "todos.db");
+    final dbPath = join(docsPath.path, 'todos.db');
     final db = await dbFactory.openDatabase(dbPath);
     return db;
   }
@@ -48,14 +39,34 @@ singleton pattern
     await store.add(_database, todo.toMap());
   }
 
-  /*Similarly, to update an existing item in the database, you can call the update()
-method of the store. The difference here is that you also need another object:
-a Finder. A Finder is a helper that you can use to search inside a store. With the
-update() method, you need to retrieve a Todo before updating it, so you need
-the Finder before you update the document. */
-
   Future updateTodo(Todo todo) async {
+    //Finder is a helper for searching a given store
     final finder = Finder(filter: Filter.byKey(todo.id));
     await store.update(_database, todo.toMap(), finder: finder);
+  }
+
+  Future deleteTodo(Todo todo) async {
+    final finder = Finder(filter: Filter.byKey(todo.id));
+    await store.delete(_database, finder: finder);
+  }
+
+  Future deleteAll() async {
+    // Clear all records from the store
+    await store.delete(_database);
+  }
+
+  Future<List<Todo>> getTodos() async {
+    await database;
+    final finder = Finder(sortOrders: [
+      SortOrder('priority'),
+      SortOrder('id'),
+    ]);
+    final todosSnapshot = await store.find(_database, finder: finder);
+    return todosSnapshot.map((snapshot) {
+      final todo = Todo.fromMap(snapshot.value);
+      //the id is automatically generated
+      todo.id = snapshot.key;
+      return todo;
+    }).toList();
   }
 }
